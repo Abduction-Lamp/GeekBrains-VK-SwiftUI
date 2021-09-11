f8e0688e3491e01658a580f8a06b863cc685dadb
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2021-09-11 17:47:33 +0300

    Begin project

---
+++ GeekBrains-VK-SwiftUI/GeekBrains-VK-SwiftUI/GeekBrains_VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeekBrains_VK_SwiftUIApp.swift
+//  GeekBrains-VK-SwiftUI
+//
+//  Created by Владимир on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct GeekBrains_VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
